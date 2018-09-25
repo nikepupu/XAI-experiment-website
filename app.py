da0bb2af0b2fd506e27cdbdab814b3b0e3639d18
Ran Gong <gongran911@163.com>
2018-09-24 19:28:58 -0700

    finish training

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask,render_template,request
 import os
+import numpy as np
+import random
+import copy
+import time
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from robot import robot
+
 app = Flask(__name__, static_url_path='/static')
 
 def getListOfFiles(dirName):
@@ -93,6 +103,280 @@ def training_phase(j, i):
 			return title, action, exp1 , exp2, img , imgtitle,ans1, ans2
 
 
+def user_study():
+
+	task_length = 6
+	episode_num = 5
+
+	# f = open("result.txt", "w")
+	r1 = robot(T=task_length*episode_num)
+	prior_list_obs = []
+	prior_list_dec = []
+	acc_obs = []
+	acc_dec = []
+	u_obs = []
+	u_dec = []
+
+	instructions = ["action: grab carrot", "action: place pan on stove", "action: turn on the stove",\
+		"action: grab cup", "action: place cup under coffee maker", "action: push brew button and get coffee"]
+	explanations = [
+		{
+			True: {
+				"obs":{True:"carrot in hand", False:"orange in hand"}, \
+				"dec":{
+					True:"preconditon: hand empty; \n  posteffect: orange in hand", \
+					False:"preconditon: hand empty; \n  posteffect: carrot in hand"
+					}
+			},
+			False: {
+				"obs":{False:"carrot in hand"}, \
+				"dec":{
+					False:"preconditon: hand empty; \n  posteffect: carrot in hand"
+					}			
+			}
+		},
+		{
+			True: {
+				"obs":{True:"pan on stove", False:"pan next to stove"}, \
+				"dec":{
+					True:"preconditon: pan in hand; \n  posteffect: pan next to stove", \
+					False:"preconditon: pan in hand; \n  posteffect: pan on stove"
+					}
+			},
+			False: {
+				"obs":{False:"pan on stove"}, \
+				"dec":{
+					False:"preconditon: pan in hand; \n  posteffect: pan on stove"
+					}			
+			}
+		},
+		{
+			True: {
+				"obs":{True:"hand near stove switch", False:"hand near oven switch"}, \
+				"dec":{
+					True:"preconditon: oven close; \n  posteffect: oven open", \
+					False:"preconditon: stove close; \n  posteffect: stove open"
+					}
+			},
+			False: {
+				"obs":{False:"hand near stove switch"}, \
+				"dec":{
+					False:"preconditon: stove close; \n  posteffect: stove open"
+					}			
+			}
+		},
+		{
+			True: {
+				"obs":{True:"cup in hand", False:"plate in hand"}, \
+				"dec":{
+					True:"preconditon: hand empty; \n  posteffect: plate in hand", \
+					False:"preconditon: hand empty; \n  posteffect: cup in hand"
+					}
+			},
+			False: {
+				"obs":{False:"cup in hand"}, \
+				"dec":{
+					False:"preconditon: hand empty; \n  posteffect: cup in hand"
+					}	
+			}
+		}, 
+		{
+			True: {
+				"obs":{True:"cup under coffee maker", False:"cup next to coffee maker"}, \
+				"dec":{
+					True:"preconditon: cup in hand; \n  posteffect: cup next to coffee maker", \
+					False:"preconditon: cup in hand; \n  posteffect: cup under coffee maker"
+					}
+			},
+			False: {
+				"obs":{False:"cup under coffee maker"}, \
+				"dec":{
+					False:"preconditon: cup in hand; \n  posteffect: cup under coffee maker"
+					}
+			}
+		}, 
+		{
+			True: {
+				"obs":{True:"coffee maker on, cup under coffee maker", \
+					False:"coffee maker off, cup under coffee maker"}, \
+				"dec":{
+					True:"preconditon: cup empty, cup under coffee maker, coffee maker off; \n  posteffect: cup filled with coffee, cup under coffee maker, coffee maker off", \
+					False:"preconditon: cup empty, cup under coffee maker, coffee maker on; \n  posteffect: cup filled with coffee, cup under coffee maker, coffee maker on"
+					}
+			},
+			False: {
+				"obs":{False:"coffee maker on, cup under coffee maker"}, \
+				"dec":{
+					False:"preconditon: cup empty, cup under coffee maker, coffee maker on; \n  posteffect: cup filled with coffee, cup under coffee maker, coffee maker on"
+					}
+			}
+		}
+
+
+	]
+
+	img_names = [
+		{True: 'images/hand_orange.PNG', False: 'images/hand_carrot.PNG'},
+		{True: 'images/not_on_stove2.PNG', False: 'images/on_stove.PNG'},
+		{True: 'images/open_oven.PNG', False: 'images/open_stove.PNG'},
+		{True: 'images/hand_plate.PNG', False: 'images/hand_cup.PNG'},
+		{True: 'images/cup_not_under_maker.PNG', False: 'images/cup_under_maker.PNG'},
+		{True: 'images/push_pour_button_off.PNG', False: 'images/push_pour_button_on.PNG'}
+		]
+
+	img_instructions = ["images/maker_off.png", "images/maker_on.png"]
+	img0 = mpimg.imread(img_instructions[0])
+	img1 = mpimg.imread(img_instructions[1])
+	f, axarr = plt.subplots(1,2)
+	axarr[0].imshow(img0)
+	axarr[0].set_title("coffee maker off")
+	axarr[1].imshow(img1)
+	axarr[1].set_title("coffee maker on")
+	plt.show(block=False)
+	raw_input("Showing states of a coffee maker. Press any button to continue.\n")
+	plt.close('all')
+
+	# img = mpimg.imread('images/hand_carrot.PNG')
+	# plt.imshow(img)
+	# plt.show(block=False)
+	# time.sleep(2.0)
+	# plt.close('all')
+
+	sub_task_length = [3,6]
+	task_name = ["cook carrot", "get coffee"]
+	num_exp = []
+
+
+	for j in range(episode_num):
+		acc_count_obs = 0
+		acc_count_dec = 0
+		u_count_obs = 0
+		u_count_dec = 0
+		sub_task_idx = 0
+		for t in range(task_length):
+			if t < sub_task_length[sub_task_idx]:
+				print "Task: "+ task_name[sub_task_idx]
+			else:
+				sub_task_idx += 1
+				print "Task: "+ task_name[sub_task_idx] 
+
+			a = r1.sample_action()
+			expl = r1.generate_exp()
+
+			ErrFlag = False
+			for key in a:
+				if key == True:
+					ErrFlag = True
+					break
+
+			ObsFlag = False
+			DecFlag = False
+			if "obs" in expl.expl_form:
+				ObsFlag = True
+			if "dec" in expl.expl_form:
+				DecFlag = True
+
+			ObsExp = ""
+			DecExp = ""
+			if ObsFlag:
+				ObsExp += explanations[t][ErrFlag]["obs"][a[0]]
+			else:
+				ObsExp = "No explanation"
+			if DecFlag:
+				DecExp += explanations[t][ErrFlag]["dec"][a[1]]
+			else:
+				DecExp = "No explanation"
+
+			print instructions[t]
+			print "Explanations: "
+			print " State: "
+			print "  "+ ObsExp
+			print " Task plan: "
+			print "  " + DecExp
+
+			resp = []
+
+			img = mpimg.imread(img_names[t][ErrFlag])
+			plt.imshow(img)
+			plt.title("Snapshot from robot's camera after the action")
+			plt.show(block=False)
+			res1 = raw_input("\n Do you think the robot's vision module is working fine (y/n)? ")
+			res2 = raw_input("Do you think the robot's task plan is right (y/n)? ")
+			plt.close('all')
+
+			if "n" in res1:
+				resp.append("obs")
+			else:
+				resp.append("ok")
+
+			if "n" in res2:
+				resp.append("dec")
+			else:
+				resp.append("ok")
+
+			if a[0] == True and resp[0] == "obs":
+				acc_count_obs += 1
+			elif a[0] == False and resp[0] == "ok":
+				acc_count_obs += 1
+
+			if a[1] == True and resp[1] == "dec":
+				acc_count_dec += 1
+			elif a[1] == False and resp[1] == "ok":
+				acc_count_dec += 1
+
+			if resp[0] == "obs":
+				u_count_obs += 1
+
+			if resp[1] == "dec":
+				u_count_dec += 1
+
+			print "\n"
+			print "\n"
+
+			r1.update_mem(resp)
+
+			prior = r1.update_prior()
+
+		num_exp.append(r1.exp_num*1.0/(task_length))
+		r1.exp_num = 0
+
+		# average prediction result
+		u_obs.append(1.0*u_count_obs/task_length)
+		u_dec.append(1.0*u_count_dec/task_length)
+
+		# human prediction accuracy
+		acc_obs.append(1.0*acc_count_obs/task_length)
+		acc_dec.append(1.0*acc_count_dec/task_length)
+
+		# machine prior
+		prior_list_obs.append(prior[0]["obs"])
+		prior_list_dec.append(prior[1]["dec"])
+
+	print "Please send back the following data and three figures (with title \"robot prior\", \"user prediction accuracy\" and \"user prediction result\")."
+	print "avg_prior_obs", prior_list_obs, "avg_prior_dec", prior_list_dec
+	print "prediction_obs", np.mean(u_obs), "prediction_dec", np.mean(u_dec)
+	print "acc_obs", np.mean(acc_obs), "acc_dec", np.mean(acc_dec)
+	print "explanation number", num_exp
+
+	plt.title("robot prior")
+	plt.plot(prior_list_obs)
+	plt.plot(prior_list_dec)
+	plt.legend(["obs", "dec"])
+	plt.show()
+
+	plt.title("user prediction accuracy")
+	plt.plot(acc_obs)
+	plt.plot(acc_dec)
+	plt.legend(["obs", "dec"])
+	plt.show()
+
+	plt.title("user prediction result")
+	plt.plot(u_obs)
+	plt.plot(u_dec)
+	plt.legend(["obs", "dec"])
+	plt.show()
+
+
 @app.route('/')
 def index():
     return render_template("intro.html")
@@ -107,7 +391,6 @@ def legends():
 
 @app.route('/example',  methods=['GET', 'POST'])
 def example():
-
 	form = request.form
 	if 'q1' in form:
 		q1 = form['q1']
@@ -119,9 +402,6 @@ def example():
 	else:
 		next_i = "0"
 		next_j = "0"
-	print next_i
-	print next_j
-
 	title, action, exp1 , exp2, img , imgtitle, ans1, ans2 = training_phase(int(next_j), int(next_i))
 	next_j = str(int(next_j) +  (int(next_i) + 1)/4)
 	next_i = str( (int(next_i)+1)%4 )
@@ -129,7 +409,7 @@ def example():
 		return render_template("example.html", title = title, action = action, exp1 = exp1 , exp2 = exp2, img = img , imgtitle = imgtitle
 		, ans1 = ans1, ans2 = ans2,next_i = next_i, next_j = next_j)
 	else:
-		return "hello world"
+		return render_template("attention.html")
 
 
 if __name__ == '__main__':
---
+++ robot.py
@@ -0,0 +1,252 @@
+import numpy as np
+import random
+import copy
+import time
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# error type (or explanation type)
+# random.seed(0)
+x_types = ["ok", "obs", "dec", "obs_dec"]
+init_err_obs = 0.8
+init_err_dec = 0.25
+init_err = [{"obs":init_err_obs, "ok":1-init_err_obs}, \
+	{"dec":init_err_dec, "ok":1-init_err_dec}]
+init_prior_obs = 0.5
+init_prior_dec = 0.5
+init_prior = [{"obs":init_prior_obs, "ok":1-init_prior_obs},\
+	{"dec":init_prior_dec, "ok":1-init_prior_dec}]
+
+class resp:
+	def __init__(self, y_i):
+		self.y_i = y_i
+		self.n_resp = len(y_i)
+
+class expl:
+	def __init__(self, expl_form):
+		self.expl_form = expl_form
+		if "_" in self.expl_form:
+			self.n_expl = 2
+		elif "ok" in self.expl_form:
+			self.n_expl = 0
+		else:
+			self.n_expl = 1
+				
+class robot:
+	def __init__(self, T, err=init_err, prior=init_prior, expl_form=x_types):
+		# dict err_type: prob
+		self.err = err
+		self.n_err = len(self.err)
+		self.expl_form = expl_form
+		self.hist_resp = []
+		self.T = T
+		self.steps = 0
+		self.exp_num = 0
+		self.gamma = 0.5
+		# size of memory, 0 means everything
+		self.memory_size = 10
+		# TO DO
+		self.err_type = ["obs", "dec"]
+		self.H = {i:0 for i in self.err_type}
+
+		# prior has the same form of err
+		self.prior = prior
+		self.x_y = []
+		for i in range(len(self.prior)):
+			x_yi = {}
+			for x in self.expl_form:
+				x_yi[x] = {}
+				for y in self.prior[i]:
+					# if no error, picks whatever x
+					if y == "ok":
+						x_yi[x][y] = 0.25
+					# if certain error, picks suitable x
+					elif y in x:
+						x_yi[x][y] = 0.4
+					else:
+						x_yi[x][y] = 0.1
+			self.x_y.append(x_yi)
+
+		self.yhat_xy = []
+		self.acc_with_x = 0.8
+		for i in range(self.n_err):
+			yhat_xyi = {}
+			for yhat in self.prior[i]:
+				yhat_xyi[yhat] = {}
+				for x in self.expl_form:
+					yhat_xyi[yhat][x] = {}
+					for y in self.prior[i]:
+						# if x is suitable, human prediction would be accurate
+						if y in x:
+							if yhat == y:
+								yhat_xyi[yhat][x][y] = self.acc_with_x
+							else:
+								yhat_xyi[yhat][x][y] = 1-self.acc_with_x
+						# if x is not good, prediction would be less accurate
+						else:
+							if yhat == y:
+								yhat_xyi[yhat][x][y] = 0.5
+							else:
+								yhat_xyi[yhat][x][y] = 0.5
+			self.yhat_xy.append(yhat_xyi)
+
+		# check if there is something wrong
+		for i in range(len(self.prior)):
+			for y in self.prior[i]:
+				cnt = 0
+				for x in self.expl_form:
+					cnt += self.x_y[i][x][y]
+				assert(cnt == 1.0)
+
+		for i in range(self.n_err):
+			for x in self.expl_form:
+				for y in self.prior[i]:
+					cnt = 0
+					for yhat in self.prior[i]:
+						cnt += self.yhat_xy[i][yhat][x][y]
+					assert(cnt == 1.0)
+
+	def sample_action(self):
+		# indicate whether certain error occurs
+		action = []
+
+		for i in range(self.n_err):
+			r = random.random()
+			prob = self.err[i]["ok"]
+			if r > prob:
+				action.append(True)
+			else:
+				action.append(False)
+
+		# print action
+		return action
+
+	def generate_exp(self):
+		# if first step, pick the most informative explanation
+		if self.steps == 0:
+			expl_form = self.expl_form[-1]
+			init_expl = expl(expl_form)
+			self.exp_num += 2
+			return init_expl
+
+		expl_form_min = None
+		cost_min = 1e5
+
+		# other wise, pick exp with minimum cost
+		for e in self.expl_form:
+
+			# # give less exp over time (add some noise to prevent user giving out the same answer)
+			# r = random.gauss(0, 0.1)
+			# cost = self.cost_trust(e) + ((0.2+r)*(1.8)**(1.0*self.steps/self.T))*self.cost_exp(e)
+
+			# same exp
+			cost = self.cost_trust(e) + 0.2*self.cost_exp(e)
+
+			# print "explanation", e, "cost", cost
+			if cost < cost_min:
+				expl_form_min = e
+				cost_min = copy.copy(cost)
+
+		expl_gen = expl(expl_form_min)
+		self.exp_num += expl_gen.n_expl
+
+		return expl_gen
+
+
+	def cost_trust(self, expl_form):
+		if len(self.hist_resp) == 0:
+			print "Error: history response is empty"
+			return 0
+
+		yhat_x = []
+		for i in range(self.n_err):
+			yhati_x = {}
+			for yhat in self.prior[i]:
+				yhati_x[yhat] = {}
+				for x in self.expl_form:
+					yhati_x[yhat][x] = 0
+					for y in self.prior[i]:
+						yhati_x[yhat][x] += self.yhat_xy[i][yhat][x][y] \
+							* self.x_y[i][x][y] * self.prior[i][y]
+
+			yhat_x.append(yhati_x)
+
+		cost_trust = 0
+
+		for t in range(len(self.hist_resp)):
+			resp = self.hist_resp[t]
+			for i in range(resp.n_resp):
+				y_i = resp.y_i[i]
+				cost_trust += -np.log(yhat_x[i][y_i][expl_form])
+
+		return 1.0*cost_trust/len(self.hist_resp)
+
+	def cost_exp(self, expl_form):
+		if "_" in expl_form:
+			return 2.0
+		elif "ok" in expl_form:
+			return 0.0
+		else:
+			return 1.0
+
+	def update_mem(self, resp_form):
+		self.steps += 1
+		response = resp(resp_form)
+		self.hist_resp.insert(0, response)
+		if len(self.hist_resp) > self.memory_size:
+			res_old = self.hist_resp.pop()
+			for i in range(res_old.n_resp):
+				y_i = res_old.y_i[i]
+				if not y_i == "ok":
+					self.H[y_i] -= 1
+
+		for i in range(response.n_resp):
+			y_i = response.y_i[i]
+			if not y_i == "ok":
+				self.H[y_i] += 1
+
+	def update_prior(self):
+		# self.yhat_xy = []
+		# for i in range(self.n_err):
+		# 	yhat_xyi = {}
+		# 	for yhat in self.prior[i]:
+		# 		yhat_xyi[yhat] = {}
+		# 		for x in self.expl_form:
+		# 			yhat_xyi[yhat][x] = {}
+		# 			for y in self.prior[i]:
+		# 				# if x is suitable, human prediction would be accurate
+		# 				if y in x:
+		# 					if yhat == y:
+		# 						yhat_xyi[yhat][x][y] = self.acc_with_x
+		# 					else:
+		# 						yhat_xyi[yhat][x][y] = 1-self.acc_with_x
+		# 				# if x is not good, prediction would be less accurate
+		# 				# either based on history or random
+		# 				else:
+		# 					if yhat == "ok":
+		# 						yhat_xyi[yhat][x][y] = 1 - self.H[self.err_type[i]]*1.0/len(self.hist_resp)
+		# 					else:
+		# 						yhat_xyi[yhat][x][y] = self.H[self.err_type[i]]*1.0/len(self.hist_resp)
+		# 	self.yhat_xy.append(yhat_xyi)
+
+		# # check if there is something wrong
+		# for i in range(len(self.prior)):
+		# 	for y in self.prior[i]:
+		# 		cnt = 0
+		# 		for x in self.expl_form:
+		# 			cnt += self.x_y[i][x][y]
+		# 		assert(cnt == 1.0)
+
+		for i in range(len(self.err_type)):
+			e_type = self.err_type[i]
+			self.prior[i][e_type] = (self.gamma*
+				self.prior[i][e_type])**(1-1.0*self.H[e_type]/len(self.hist_resp))
+			# self.prior[i][e_type] = (self.gamma*
+			# 	self.prior[i][e_type])**(1-1.0*self.H[e_type]/self.memory_size)
+			self.prior[i]["ok"] = 1-self.prior[i][e_type]
+
+		# print "prior", self.prior
+		return self.prior
+
+
