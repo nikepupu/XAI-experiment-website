33d2e0dcb109f461c83df299a2110e56451ebc5f
Ran Gong <gongran911@163.com>
2018-09-29 17:15:25 -0700

    test on aws

--- app.py
+++ app.py
@@ -306,7 +306,7 @@ def user_study(j, t,sub_task_idx, r1):
 
 @app.route('/',methods=['GET', 'POST'])
 def index():
-    idnum = "1"#str(uuid.uuid4())
+    idnum = str(uuid.uuid4())
     cursor = doc.find({idnum: {"$exists": True}}).limit(1)
     if cursor.count() == 0:
 		key = {"user_id_xai":idnum}
